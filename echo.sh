b094f739eb8e491599e28da7c9aa572f7d2b0309
favlh <firjat809@gmail.com>
2024-11-02 17:53:06 +0700

    example of bashscript using echo

---
+++ echo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+echo "This script is for examples using echo in bashscript"
+
+echo "Linux is awesome, is'nt?"
+echo "I love linux, ofc"
+echo "Everything is fun, if you understand it"
